9cd79f3b961a477bfa552d0bb8534013c3917f3b
DahanSimon <dahan.simon@hotmail.fr>
2020-09-23 17:21:25 +0200

    Initial Commit

---
+++ Le Baluchon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Le Baluchon
+//
+//  Created by Simon Dahan on 23/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
