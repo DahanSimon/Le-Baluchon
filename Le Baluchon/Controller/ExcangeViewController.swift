1111c4c7be30214654fc3df69b09fd8a7862aed1
DahanSimon <dahan.simon@hotmail.fr>
2020-09-23 19:30:20 +0200

    - Currencie's layout view done - Creation of ExchangeViewController

---
+++ Le Baluchon/Controller/ExcangeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ExcangeViewController.swift
+//  Le Baluchon
+//
+//  Created by Simon Dahan on 23/09/2020.
+//
+
+import UIKit
+
+class ExcangeViewController: UIViewController {
+
+    @IBOutlet weak var translateButton: UIButton!
+}
